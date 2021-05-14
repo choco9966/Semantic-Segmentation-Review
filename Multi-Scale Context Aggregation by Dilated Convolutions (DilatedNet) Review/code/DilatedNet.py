7870c8109b0b05060307a83bf36981a9ddb21b0b
kimhyeonwoo <37744629+choco9966@users.noreply.github.com>
2021-05-14 20:00:36 +0900

    Update DilatedNet.py

--- Multi-Scale Context Aggregation by Dilated Convolutions (DilatedNet) Review/code/DilatedNet.py
+++ Multi-Scale Context Aggregation by Dilated Convolutions (DilatedNet) Review/code/DilatedNet.py
@@ -3,9 +3,13 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 def conv_relu(in_ch, out_ch, size=3, rate=1):
-    conv_relu = nn.Sequential(nn.Conv2d(in_ch, out_ch, kernel_size=size, 
-                                    stride=1, padding=rate, dilation=rate),
-                                 nn.ReLU())
+    conv_relu = nn.Sequential(nn.Conv2d(in_channels=in_ch, 
+                                        out_channels=out_ch,
+                                        kernel_size=size, 
+                                        stride=1, 
+                                        padding=rate, 
+                                        dilation=rate),
+                              nn.ReLU())
     return conv_relu
 
 
@@ -13,21 +17,21 @@ class VGG16(nn.Module):
     def __init__(self):
         super(VGG16, self).__init__()
         self.features1 = nn.Sequential(conv_relu(3, 64, 3, 1),
-                                      conv_relu(64, 64, 3, 1),
-                                      nn.MaxPool2d(2, stride=2, padding=0))
+                                       conv_relu(64, 64, 3, 1),
+                                       nn.MaxPool2d(2, stride=2, padding=0))
         self.features2 = nn.Sequential(conv_relu(64, 128, 3, 1),
-                                      conv_relu(128, 128, 3, 1),
-                                      nn.MaxPool2d(2, stride=2, padding=0))
+                                       conv_relu(128, 128, 3, 1),
+                                       nn.MaxPool2d(2, stride=2, padding=0))
         self.features3 = nn.Sequential(conv_relu(128, 256, 3, 1),
-                                      conv_relu(256, 256, 3, 1),
-                                      conv_relu(256, 256, 3, 1),
-                                      nn.MaxPool2d(2, stride=2, padding=0))
+                                       conv_relu(256, 256, 3, 1),
+                                       conv_relu(256, 256, 3, 1),
+                                       nn.MaxPool2d(2, stride=2, padding=0))
         self.features4 = nn.Sequential(conv_relu(256, 512, 3, 1),
-                                      conv_relu(512, 512, 3, 1),
-                                      conv_relu(512, 512, 3, 1),
+                                       conv_relu(512, 512, 3, 1),
+                                       conv_relu(512, 512, 3, 1))
         self.features5 = nn.Sequential(conv_relu(512, 512, 3, rate=2),
-                                      conv_relu(512, 512, 3, rate=2),
-                                      conv_relu(512, 512, 3, rate=2))
+                                       conv_relu(512, 512, 3, rate=2),
+                                       conv_relu(512, 512, 3, rate=2))
                                        
     def forward(self, x):
         out = self.features1(x)
@@ -40,17 +44,18 @@ class VGG16(nn.Module):
 class classifier(nn.Module):
     def __init__(self, num_classes): 
         super(classifier, self).__init__()
-        self.classifier = nn.Sequential(nn.Conv2d(512,4096,kernel_size=7,dilation=4,padding=12), 
-                                       nn.ReLU(),
-                                       nn.Dropout2d(0.5), 
-                                       nn.Conv2d(1024, 1024, 1), 
-                                       nn.ReLU(), 
-                                       nn.Dropout2d(0.5), 
-                                       nn.Conv2d(1024, num_classes, kernel_size=1)
-                                       )
+        self.classifier = nn.Sequential(nn.Conv2d(512, 4096, kernel_size=7, dilation=4, padding=12), 
+                                        nn.ReLU(),
+                                        nn.Dropout2d(0.5),
+                                        nn.Conv2d(4096, 4096, kernel_size=1),
+                                        nn.ReLU(),
+                                        nn.Dropout2d(0.5),
+                                        nn.Conv2d(4096, num_classes, kernel_size=1)
+                                        )
+
     def forward(self, x): 
         out = self.classifier(x)
-        return out 
+        return out
 
 class BasicContextModule(nn.Module):
     def __init__(self, num_classes):
@@ -69,13 +74,13 @@ class BasicContextModule(nn.Module):
     def forward(self, x): 
         
         out = self.layer1(x)
-        out = self.layer2(x)
-        out = self.layer3(x)
-        out = self.layer4(x)
-        out = self.layer5(x)
-        out = self.layer6(x)
-        out = self.layer7(x)
-        out = self.layer8(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
         
         return out
     
@@ -96,5 +101,5 @@ class DilatedNet(nn.Module):
         x = self.backbone(x)
         x = self.classifier(x)
         x = self.context_module(x)
-        x = self.deconv(x)
-        return x
+        out = self.deconv(x)
+        return out
