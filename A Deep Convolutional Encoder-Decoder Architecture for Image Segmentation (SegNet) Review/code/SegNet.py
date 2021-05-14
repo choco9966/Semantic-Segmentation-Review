a2426ab4125bb2acadf3d321bee26bb2e09360e2
kimhyeonwoo <37744629+choco9966@users.noreply.github.com>
2021-05-14 19:59:40 +0900

    Update SegNet.py

--- A Deep Convolutional Encoder-Decoder Architecture for Image Segmentation (SegNet) Review/code/SegNet.py
+++ A Deep Convolutional Encoder-Decoder Architecture for Image Segmentation (SegNet) Review/code/SegNet.py
@@ -1,152 +1,141 @@
 import torch
 import torch.nn as nn
-from torchvision import models
-
-
 
 class SegNet(nn.Module):
-    def __init__(self, num_classes=21, init_weights=True):
+    def __init__(self, num_classes=12, init_weights=True):
         super(SegNet, self).__init__()
-        self.relu = nn.ReLU(inplace=True)
-        
         def CBR(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
-            
-            return nn.Sequential(
-                nn.Conv2d(in_channels=in_channels, 
-                          out_channels=out_channels,
-                          kernel_size=kernel_size, 
-                          stride=stride, 
-                          padding=padding),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU())
+            layers = []
+            layers += [nn.Conv2d(in_channels=in_channels, 
+                                 out_channels=out_channels,
+                                 kernel_size=kernel_size, 
+                                 stride=stride, 
+                                 padding=padding)]
+            layers += [nn.BatchNorm2d(num_features=out_channels)]
+            layers += [nn.ReLU()]
 
+            cbr = nn.Sequential(*layers)
+            return cbr
         
-        '''
-        input: "data"
-        input_dim: 1
-        input_dim: 3
-        input_dim: 224
-        input_dim: 224
-        '''
-        
-        # 224 x 224
-        # conv1
-        self.conv1_1 = CBR(3, 64, 3, 1, 1)
-        self.conv1_2 = CBR(64, 64, 3, 1, 1)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True, return_indices=True) # 1/2
-        
-        # 112 x 112
+        # conv1 
+        self.cbr1_1 = CBR(3, 64, 3, 1, 1)
+        self.cbr1_2 = CBR(64, 64, 3, 1, 1)
+        self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True, return_indices=True) 
+
         # conv2 
-        self.conv2_1 = CBR(64, 128, 3, 1, 1)
-        self.conv2_2 = CBR(128, 128, 3, 1, 1)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True, return_indices=True) # 1/4
-        
-        # 56 x 56
+        self.cbr2_1 = CBR(64, 128, 3, 1, 1)
+        self.cbr2_2 = CBR(128, 128, 3, 1, 1)
+        self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True, return_indices=True) 
+
         # conv3
-        self.conv3_1 = CBR(128, 256, 3, 1, 1)
-        self.conv3_2 = CBR(256, 256, 3, 1, 1)
-        self.conv3_3 = CBR(256, 256, 3, 1, 1)        
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True, return_indices=True) # 1/8
-        
-        # 28 x 28
+        self.cbr3_1 = CBR(128, 256, 3, 1, 1)
+        self.cbr3_2 = CBR(256, 256, 3, 1, 1)
+        self.cbr3_3 = CBR(256, 256, 3, 1, 1)
+        self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True, return_indices=True) 
+
         # conv4
-        self.conv4_1 = CBR(256, 512, 3, 1, 1)
-        self.conv4_2 = CBR(512, 512, 3, 1, 1)
-        self.conv4_3 = CBR(512, 512, 3, 1, 1)        
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True, return_indices=True) # 1/16
-        
-        # 14 x 14
+        self.cbr4_1 = CBR(256, 512, 3, 1, 1)
+        self.cbr4_2 = CBR(512, 512, 3, 1, 1)
+        self.cbr4_3 = CBR(512, 512, 3, 1, 1)
+        self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True, return_indices=True) 
+
         # conv5
-        self.conv5_1 = CBR(512, 512, 3, 1, 1)
-        self.conv5_2 = CBR(512, 512, 3, 1, 1)
-        self.conv5_3 = CBR(512, 512, 3, 1, 1)
-        self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True, return_indices=True)
+        self.cbr5_1 = CBR(512, 512, 3, 1, 1)
+        self.cbr5_2 = CBR(512, 512, 3, 1, 1)
+        self.cbr5_3 = CBR(512, 512, 3, 1, 1)
+        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True, return_indices=True) 
         
-        # 14 x 14
-        # unpool5
+        # deconv5
         self.unpool5 = nn.MaxUnpool2d(2, stride=2)
-        self.deconv5_1 = CBR(512, 512, 3, 1, 1)
-        self.deconv5_2 = CBR(512, 512, 3, 1, 1)
-        self.deconv5_3 = CBR(512, 512, 3, 1, 1)
-        
-        # 28 x 28
-        # unpool4
+        self.dcbr5_3 = CBR(512, 512, 3, 1, 1)
+        self.dcbr5_2 = CBR(512, 512, 3, 1, 1)
+        self.dcbr5_1 = CBR(512, 512, 3, 1, 1)
+
+        # deconv4 
         self.unpool4 = nn.MaxUnpool2d(2, stride=2)
-        self.deconv4_1 = CBR(512, 512, 3, 1, 1)
-        self.deconv4_2 = CBR(512, 512, 3, 1, 1)
-        self.deconv4_3 = CBR(512, 256, 3, 1, 1)        
+        self.dcbr4_3 = CBR(512, 512, 3, 1, 1)
+        self.dcbr4_2 = CBR(512, 512, 3, 1, 1)
+        self.dcbr4_1 = CBR(512, 256, 3, 1, 1)
 
-        # 56 x 56        
-        # unpool3
+        # deconv3
         self.unpool3 = nn.MaxUnpool2d(2, stride=2)
-        self.deconv3_1 = CBR(256, 256, 3, 1, 1)
-        self.deconv3_2 = CBR(256, 256, 3, 1, 1)
-        self.deconv3_3 = CBR(256, 128, 3, 1, 1)                          
-        
-        # 112 x 112         
-        # unpool2
-        self.unpool2 = nn.MaxUnpool2d(2, stride=2)
-        self.deconv2_1 = CBR(128, 128, 3, 1, 1)
-        self.deconv2_2 = CBR(128, 64, 3, 1, 1)
+        self.dcbr3_3 = CBR(256, 256, 3, 1, 1)
+        self.dcbr3_2 = CBR(256, 256, 3, 1, 1)
+        self.dcbr3_1 = CBR(256, 128, 3, 1, 1)
 
-        # 224 x 224        
-        # unpool1
+        # deconv2
+        self.unpool2 = nn.MaxUnpool2d(2, stride=2)
+        self.dcbr2_2 = CBR(128, 128, 3, 1, 1)
+        self.dcbr2_1 = CBR(128, 64, 3, 1, 1)
+
+        # deconv1
         self.unpool1 = nn.MaxUnpool2d(2, stride=2)
         self.deconv1_1 = CBR(64, 64, 3, 1, 1)
         # Score
-        self.score_fr = nn.Conv2d(64, num_classes, 3, 1, 1, 1)
+        # self.score_fr = nn.Conv2d(64, num_classes, kernel_size = 1)
+        self.score_fr = nn.Conv2d(64, num_classes, kernel_size = 3, padding=1)
         
         if init_weights:
             self._initialize_weights()
-    
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.xavier_uniform_(m.weight)
+
+                if m.bias is not None:
+                    torch.nn.init.zeros_(m.bias)
+                    
     def forward(self, x):
-        
-        h = self.conv1_1(x)
-        h = self.conv1_2(h)
+        h = self.cbr1_1(x)
+        h = self.cbr1_2(h)
+        dim1 = h.size()
         h, pool1_indices = self.pool1(h)
         
-        h = self.conv2_1(h)
-        h = self.conv2_2(h)
+        h = self.cbr2_1(h)
+        h = self.cbr2_2(h)
+        dim2 = h.size()
         h, pool2_indices = self.pool2(h)
         
-        h = self.conv3_1(h)
-        h = self.conv3_2(h)
-        h = self.conv3_3(h)        
+        h = self.cbr3_1(h)
+        h = self.cbr3_2(h)
+        h = self.cbr3_3(h)
+        dim3 = h.size()
         h, pool3_indices = self.pool3(h)
         
-        h = self.conv4_1(h)
-        h = self.conv4_2(h)
-        h = self.conv4_3(h)        
-        h, pool4_indices = self.pool4(h) 
+        h = self.cbr4_1(h)
+        h = self.cbr4_2(h)
+        h = self.cbr4_3(h)
+        dim4 = h.size()
+        h, pool4_indices = self.pool4(h)
         
-        h = self.conv5_1(h)
-        h = self.conv5_2(h)
-        h = self.conv5_3(h)        
+        h = self.cbr5_1(h)
+        h = self.cbr5_2(h)
+        h = self.cbr5_3(h)
+        dim5 = h.size()
         h, pool5_indices = self.pool5(h)
         
+        h = self.unpool5(h, pool5_indices, output_size = dim5)
+        h = self.dcbr5_3(h)
+        h = self.dcbr5_2(h)
+        h = self.dcbr5_1(h)
         
-        h = self.unpool5(h, pool5_indices)
-        h = self.deconv5_1(h)        
-        h = self.deconv5_2(h)                
-        h = self.deconv5_3(h)                
-
-        h = self.unpool4(h, pool4_indices)
-        h = self.deconv4_1(h)        
-        h = self.deconv4_2(h)                
-        h = self.deconv4_3(h)                       
-
-        h = self.unpool3(h, pool3_indices)
-        h = self.deconv3_1(h)        
-        h = self.deconv3_2(h)                
-        h = self.deconv3_3(h)                            
+        h = self.unpool4(h, pool4_indices, output_size = dim4)
+        h = self.dcbr4_3(h)
+        h = self.dcbr4_2(h)
+        h = self.dcbr4_1(h)
         
-        h = self.unpool2(h, pool2_indices)
-        h = self.deconv2_1(h)        
-        h = self.deconv2_2(h)                                         
-
-        h = self.unpool1(h, pool1_indices)
-        h = self.deconv1_1(h)                                        
-            
-        h = self.score_fr(h)           
+        h = self.unpool3(h, pool3_indices, output_size = dim3)
+        h = self.dcbr3_3(h)
+        h = self.dcbr3_2(h)
+        h = self.dcbr3_1(h)
         
-        return h
+        h = self.unpool2(h, pool2_indices, output_size = dim2)
+        h = self.dcbr2_2(h)
+        h = self.dcbr2_1(h)
+        
+        h = self.unpool1(h, pool1_indices, output_size = dim1)
+        h = self.deconv1_1(h)
+        out = self.score_fr(h) 
+        
+        return out
